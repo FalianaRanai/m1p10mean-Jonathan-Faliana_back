c8e1643dbe2bf879f874ca2b36fb58c0fbe66c98
Jonathan <stevenjacob906@gmail.com>
2024-02-06 11:52:50 +0300

    Update employe.controller.js

--- controllers/employe.controller.js
+++ controllers/employe.controller.js
@@ -14,200 +14,213 @@ const deleteFile = require("@utils/deleteFile.util");
 const Tachedb = require("../models/tache.model");
 
 exports.getEmploye = (req, res) => {
-  const functionName = "getEmploye";
-  try {
-    const { id } = req.params;
-    verifyArgumentExistence(["id"], req.params);
+    const functionName = "getEmploye";
+    try {
+        const { id } = req.params;
+        verifyArgumentExistence(["id"], req.params);
 
-    Employedb.findById(id)
-      .populate({ path: "user", populate: { path: "role" } })
-      .populate({
-        path: "listeTaches",
-        populate: [
-          {
-            path: "employe",
-            populate: { path: "user", populate: { path: "role" } },
-          },
-          { path: "service" },
-          { path: "statut" },
-        ],
-      })
-      .then((data) => {
-        sendSuccessResponse(res, data, controllerName, functionName);
-      })
-      .catch((err) => {
+        Employedb.findById(id)
+            .populate({ path: "user", populate: { path: "role" } })
+            .populate({
+                path: "listeTaches",
+                populate: [
+                    {
+                        path: "employe",
+                        populate: { path: "user", populate: { path: "role" } },
+                    },
+                    { path: "service" },
+                    { path: "statut" },
+                ],
+            })
+            .then((data) => {
+                sendSuccessResponse(res, data, controllerName, functionName);
+            })
+            .catch((err) => {
+                sendErrorResponse(res, err, controllerName, functionName);
+            });
+    } catch (err) {
         sendErrorResponse(res, err, controllerName, functionName);
-      });
-  } catch (err) {
-    sendErrorResponse(res, err, controllerName, functionName);
-  }
+    }
 };
 
 exports.getListeEmploye = (req, res) => {
-  const functionName = "getListeEmploye";
-  try {
-    Employedb.find({})
-      .populate({ path: "user", populate: { path: "role" } })
-      .populate({
-        path: "listeTaches",
-        populate: [
-          {
-            path: "employe",
-            populate: { path: "user", populate: { path: "role" } },
-          },
-          { path: "service" },
-          { path: "statut" },
-        ],
-      })
-      .then((data) => {
-        sendSuccessResponse(res, data, controllerName, functionName);
-      })
-      .catch((err) => {
+    const functionName = "getListeEmploye";
+    try {
+        Employedb.find({})
+            .populate({ path: "user", populate: { path: "role" } })
+            .populate({
+                path: "listeTaches",
+                populate: [
+                    {
+                        path: "employe",
+                        populate: { path: "user", populate: { path: "role" } },
+                    },
+                    { path: "service" },
+                    { path: "statut" },
+                ],
+            })
+            .then((data) => {
+                sendSuccessResponse(res, data, controllerName, functionName);
+            })
+            .catch((err) => {
+                sendErrorResponse(res, err, controllerName, functionName);
+            });
+    } catch (err) {
         sendErrorResponse(res, err, controllerName, functionName);
-      });
-  } catch (err) {
-    sendErrorResponse(res, err, controllerName, functionName);
-  }
+    }
 };
 
 exports.addEmploye = async (req, res) => {
-  const functionName = "addEmploye";
-  const session = await mongoose.startSession();
-  session.startTransaction();
-  try {
-    const { nomEmploye, prenomEmploye, email, password, confirmPassword } =
-      req.body;
+    const functionName = "addEmploye";
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const { nomEmploye, prenomEmploye, email, password, confirmPassword } =
+            req.body;
 
-    verifyArgumentExistence(
-      ["nomEmploye", "prenomEmploye", "email", "password", "confirmPassword"],
-      req.body
-    );
+        verifyArgumentExistence(
+            ["nomEmploye", "prenomEmploye", "email", "password", "confirmPassword"],
+            req.body
+        );
 
-    if ((password && !confirmPassword) || (!password && confirmPassword)) {
-      throw new Error("Veillez remplir les champs de mot de passe");
-    }
-    if (confirmPassword && confirmPassword !== password) {
-      throw new Error("Les mots de passes ne correspondent pas");
-    }
+        if ((password && !confirmPassword) || (!password && confirmPassword)) {
+            throw new Error("Veillez remplir les champs de mot de passe");
+        }
+        if (confirmPassword && confirmPassword !== password) {
+            throw new Error("Les mots de passes ne correspondent pas");
+        }
 
-    let role = await Roledb.findOne({ nomRole: "Employe" });
-    if (!role) {
-      await new Roledb({ nomRole: "Employe" }).save();
-      role = await Roledb.findOne({ nomRole: "Employe" });
-    }
+        let role = await Roledb.findOne({ nomRole: "Employe" });
+        if (!role) {
+            await new Roledb({ nomRole: "Employe" }).save();
+            role = await Roledb.findOne({ nomRole: "Employe" });
+        }
 
-    const newDataUser = {
-      email: email,
-      password: bcrypt.hashSync(password, 10),
-      role: new ObjectId(role._id),
-    };
-
-    const dataUserToInsert = new Userdb(newDataUser);
-    dataUserToInsert
-      .save({ session })
-      .then(async (newUser) => {
-        let nomFichier = await writeFile(req, "Employe");
-
-        const newData = {
-          nomEmploye: nomEmploye,
-          prenomEmploye: prenomEmploye,
-          user: new ObjectId(newUser._id),
-          image: nomFichier ? nomFichier : undefined,
+        const newDataUser = {
+            email: email,
+            password: bcrypt.hashSync(password, 10),
+            role: new ObjectId(role._id),
         };
 
-        const dataToInsert = new Employedb(newData);
-        dataToInsert
-          .save({ session })
-          .then(async (data) => {
-            sendSuccessResponse(
-              res,
-              data,
-              controllerName,
-              functionName,
-              session
-            );
-          })
-          .catch((err) => {
-            sendErrorResponse(res, err, controllerName, functionName, session);
-          });
-      })
-      .catch((err) => {
+        const dataUserToInsert = new Userdb(newDataUser);
+        dataUserToInsert
+            .save({ session })
+            .then(async (newUser) => {
+                let nomFichier = await writeFile(req, "Employe");
+
+                const newData = {
+                    nomEmploye: nomEmploye,
+                    prenomEmploye: prenomEmploye,
+                    user: new ObjectId(newUser._id),
+                    image: nomFichier ? nomFichier : undefined,
+                };
+
+                const dataToInsert = new Employedb(newData);
+                dataToInsert
+                    .save({ session })
+                    .then(async (data) => {
+                        sendSuccessResponse(
+                            res,
+                            data,
+                            controllerName,
+                            functionName,
+                            session
+                        );
+                    })
+                    .catch((err) => {
+                        sendErrorResponse(res, err, controllerName, functionName, session);
+                    });
+            })
+            .catch((err) => {
+                sendErrorResponse(res, err, controllerName, functionName, session);
+            });
+    } catch (err) {
         sendErrorResponse(res, err, controllerName, functionName, session);
-      });
-  } catch (err) {
-    sendErrorResponse(res, err, controllerName, functionName, session);
-  }
+    }
 };
 
 exports.updateEmploye = async (req, res) => {
-  const functionName = "updateEmploye";
-  const session = await mongoose.startSession();
-  session.startTransaction();
-  try {
-    const { id } = req.params;
-    const { nomEmploye, prenomEmploye, user } = req.body;
+    const functionName = "updateEmploye";
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const { id } = req.params;
+        const { nomEmploye, prenomEmploye, user, email, password } = req.body;
 
-    verifyArgumentExistence(["id"], req.params);
-    verifyArgumentExistence(["nomEmploye", "prenomEmploye", "user"], req.body);
+        verifyArgumentExistence(["id"], req.params);
+        verifyArgumentExistence(["nomEmploye", "prenomEmploye", "user", "email", "password"], req.body);
 
-    let nomFichier = await writeFile(req, "Employe");
-    const newData = {
-      nomEmploye: nomEmploye,
-      prenomEmploye: prenomEmploye,
-      user: new ObjectId(user),
-      image: nomFichier ? nomFichier : undefined,
-    };
+        let nomFichier = await writeFile(req, "Employe");
+        const newData = {
+            nomEmploye: nomEmploye,
+            prenomEmploye: prenomEmploye,
+            user: new ObjectId(user),
+            image: nomFichier ? nomFichier : undefined,
+        };
 
-    Employedb.findByIdAndUpdate(new ObjectId(id), newData, {
-      session,
-    })
-      .then(async (data) => {
-        if (nomFichier && data.image != "default.webp") {
-          await deleteFile("Employe", data.image);
-        }
-        sendSuccessResponse(res, data, controllerName, functionName, session);
+        const newUserData = {
+            email: email,
+            password: password
+        };
 
-        // sendSuccessResponse(res, data, controllerName, functionName, session);
-      })
-      .catch(async (err) => {
+        Userdb.findByIdAndUpdate(new ObjectId(user), newUserData, {
+            session,
+        })
+            .then(() => {
+                Employedb.findByIdAndUpdate(new ObjectId(id), newData, {
+                    session,
+                })
+                    .then(async (data) => {
+                        if (nomFichier && data.image != "default.webp") {
+                            await deleteFile("Employe", data.image);
+                        }
+                        sendSuccessResponse(res, data, controllerName, functionName, session);
+                    })
+                    .catch(async (err) => {
+                        sendErrorResponse(res, err, controllerName, functionName, session);
+                    });
+
+            })
+
+            .catch(async (err) => {
+                sendErrorResponse(res, err, controllerName, functionName, session);
+            });
+    } catch (err) {
         sendErrorResponse(res, err, controllerName, functionName, session);
-      });
-  } catch (err) {
-    sendErrorResponse(res, err, controllerName, functionName, session);
-  }
+    }
 };
 
 exports.deleteEmploye = async (req, res) => {
-  const functionName = "deleteEmploye";
-  const session = await mongoose.startSession();
-  session.startTransaction();
-  try {
-    const { id } = req.params;
-    verifyArgumentExistence(["id"], req.params);
+    const functionName = "deleteEmploye";
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const { id } = req.params;
+        verifyArgumentExistence(["id"], req.params);
 
-    Employedb.findByIdAndDelete(new ObjectId(id), { session })
-      .then(async (data) => {
-        await Userdb.deleteOne({ _id: new ObjectId(data.user) }, { session });
-        await UserTokendb.deleteMany(
-          { user: new ObjectId(data.user) },
-          { session }
-        );
-        await Tachedb.deleteMany(
-          { employe: new ObjectId(data._id) },
-          { session }
-        );
+        Employedb.findByIdAndDelete(new ObjectId(id), { session })
+            .then(async (data) => {
+                await Userdb.deleteOne({ _id: new ObjectId(data.user) }, { session });
+                await UserTokendb.deleteMany(
+                    { user: new ObjectId(data.user) },
+                    { session }
+                );
+                await Tachedb.deleteMany(
+                    { employe: new ObjectId(data._id) },
+                    { session }
+                );
 
-        if (data.image != "default.webp") {
-          await deleteFile("Employe", data.image);
-        }
-        sendSuccessResponse(res, data, controllerName, functionName, session);
+                if (data.image != "default.webp") {
+                    await deleteFile("Employe", data.image);
+                }
+                sendSuccessResponse(res, data, controllerName, functionName, session);
 
-        // sendSuccessResponse(res, data, controllerName, functionName, session);
-      })
-      .catch(async (err) => {
+                // sendSuccessResponse(res, data, controllerName, functionName, session);
+            })
+            .catch(async (err) => {
+                sendErrorResponse(res, err, controllerName, functionName, session);
+            });
+    } catch (err) {
         sendErrorResponse(res, err, controllerName, functionName, session);
-      });
-  } catch (err) {
-    sendErrorResponse(res, err, controllerName, functionName, session);
-  }
+    }
 };
