0af16979e01a4631a8dd747f48a79c801624c9e0
Faliana <faliana23@gmail.com>
2024-02-22 17:02:15 +0300

    rappel rdv

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -7,6 +7,7 @@ const upload = multer();
 
 router.get("/getClient/:id", ClientController.getClient);
 router.get("/getListeClient", ClientController.getListeClient);
+router.get("/getRappelRendezVous/:client", ClientController.getRappelRendezVous);
 router.put("/savePreference/:id", ClientController.savePreference);
 router.post("/addClient", upload.fields([{ name: 'file', maxCount: 1 }]), ClientController.addClient);
 router.put("/updateClient/:id", upload.fields([{ name: 'file', maxCount: 1 }]), ClientController.updateClient);
